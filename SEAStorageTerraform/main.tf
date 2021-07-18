60dc10bc9eb945cfb0dd7551c04eccd15ea03ffd
Anirban Roy <87340150+AnirbanRoyIT@users.noreply.github.com>
2021-07-18 23:58:35 +0530

    Delete SEAStorageTerraform directory

--- SEAStorageTerraform/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_storage_account" "storageaccount" {
-  name                     = var.name
-  resource_group_name      = var.rgroup
-  location                 = var.region
-  account_tier             = "Standard"
-  account_replication_type = var.sku
-
-}
--- SEAStorageTerraform/output.txt
+++
@@ -1,4 +0,0 @@
-output "detailsofdeploymnt" {
-  value = azurerm_storage_account.storageaccount.primary_blob_endpoint
-  description = "storage account endpont URL"
-}
